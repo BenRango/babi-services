d447b018bcec92b7d23996e9d144c644f9419e54
amari <lattroange@gmail.com>
2026-07-09 02:35:48 +0000

    feat: add Input component and integrate it into the main index file
    - Created a new Input component for user input with customizable props.
    - Integrated the Input component into the main index file for phone number entry.
    - Added a new chat component file with initial structure commented out.
    - Updated theme constants for consistent styling and added styles for input elements.
    - Modified tsconfig to ensure JSX support for React.

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -3,9 +3,10 @@ import { StyleSheet, TextInput } from 'react-native'
 import { View } from 'react-native'
 
 interface InputProps {
-  value ?: string
+  value : string
   text : string
   placeholder ?: string
+  onChangeText : (text: string) => void
 
 }
 
@@ -14,25 +15,31 @@ export default function Input(props: InputProps) {
     <View style = {[styles.container]}>
       <TextInput 
         style = {styles.input}
+        keyboardType = "numeric"
         placeholder = {props.placeholder}
         placeholderTextColor = "#A0A0A0"
         value={props.value}
+        onChangeText={props.onChangeText}
       />
       
     </View>
   )
 }
 
+
+
 const styles = StyleSheet.create({
   container: {
     padding: 25,
     borderRadius: 8,
     marginBottom: 16,
+     width: "70%",
+
   },
   input: {
     padding: 20,
     borderRadius: 15,
-    backgroundColor: '#F9FAFB',
+    backgroundColor: '#073d73',
 
   },
  
