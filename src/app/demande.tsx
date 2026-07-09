bd7cdc211ffd7a8b86f620ced2ea942db776ef68
Benrango <srangoelie@gmail.com>
2026-07-09 03:45:22 +0000

    feat: add service selection buttons and update input fields in demande screen

--- src/app/demande.tsx
+++ src/app/demande.tsx
@@ -7,6 +7,30 @@ import { SafeAreaView } from "react-native-safe-area-context"
 export default function demande() {
     const [location, setLocation] = useState<string>("")
     const [description, setDescription] = useState<string>("")
+    const [services2, setServices2] = useState<ServiceButtonProps[]>([
+        {
+        title: "Plomberie",
+        logoSource: require("@/assets/icons/water-tap-plumber.svg"),
+        selected: true,
+        onPress: () => {
+            console.log("Plomberie")
+        }
+        },
+        {
+        title: "Electricité",
+        logoSource: require("@/assets/icons/electricity-technology.svg"),
+        onPress: () => {
+            console.log("Electricité")
+        }
+        },
+        {
+        title: "Menuisier",
+        logoSource: require("@/assets/icons/carpenter.svg"),
+        onPress: () => {
+            console.log("Menuisier")
+        }
+        }]
+    )
     const [services, setServices] = useState<ServiceButtonProps[]>([
     {
       title: "Plomberie",
@@ -86,6 +110,24 @@ export default function demande() {
             value={location}
             onChangeText={(text)=> setLocation(text)}
         />
+        <Text style={styles.sectionTitle}>Date souhaitée*</Text>
+        <InputComponent 
+            value={location}
+            onChangeText={(text)=> setLocation(text)}
+        />
+
+        <Text style={styles.sectionTitle}>Mode facturation*</Text>
+        <View style={styles.container}>
+            {services2.map((service) => (
+                <ServiceButton
+                    key={service.title} 
+                    title={service.title} 
+                    logoSource={service.logoSource} 
+                    onPress={() => changeSelectedService(service)} 
+                    selected={service.selected}
+                />
+            ))}
+        </View>
     </SafeAreaView>
   )
 }
