fe0ad4e66687efa3f6f5d7cf5b4c196ec9d25950
jochris <chris.johan.225@gmail.com>
2026-07-10 20:07:47 +0200

    feat add struturel folder for debut project and login screen and page navigation

---
+++ src/app/(onboarding)/welcome.tsx
@@ -0,0 +1,103 @@
+import ContinuerButton from "@/components/continuerButton";
+import { ServiceButtonProps } from "@/components/serviceButton";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 100,
+    justifyContent: "space-around",
+    alignItems: "center",
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+});
+
+function index() {
+  const router = useRouter();
+  const [services, setServices] = useState<ServiceButtonProps[]>([
+    {
+      title: "Plomberie",
+      logoSource: require("@/assets/icons/water-tap-plumber.svg"),
+      selected: true,
+      onPress: () => {
+        console.log("Plomberie");
+      },
+    },
+    {
+      title: "Electricité",
+      logoSource: require("@/assets/icons/electricity-technology.svg"),
+      onPress: () => {
+        console.log("Electricité");
+      },
+    },
+    {
+      title: "Menuisier",
+      logoSource: require("@/assets/icons/carpenter.svg"),
+      onPress: () => {
+        console.log("Menuisier");
+      },
+    },
+    {
+      title: "Menuisier2",
+      logoSource: require("@/assets/icons/carpenter.svg"),
+      onPress: () => {
+        console.log("Menuisier");
+      },
+    },
+    {
+      title: "Menuisier3",
+      logoSource: require("@/assets/icons/carpenter.svg"),
+      onPress: () => {
+        console.log("Menuisier");
+      },
+    },
+    {
+      title: "Climatisation",
+      logoSource: require("@/assets/icons/breeze.svg"),
+      onPress: () => {
+        console.log("Climatisation");
+      },
+    },
+  ]);
+
+  const changeSelectedService = (service: ServiceButtonProps) => {
+    setServices(
+      services.map((s) => ({
+        ...s,
+        selected: s.title === service.title ? !s.selected : s.selected,
+      })),
+    );
+    console.log(services);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {/*services.map((service) => (
+        <ServiceButton
+          key={service.title}
+          title={service.title}
+          logoSource={service.logoSource}
+          onPress={() => changeSelectedService(service)}
+          selected={service.selected}
+        />
+      ))*/}
+      <ContinuerButton
+        filled
+        text={"Continuer"}
+        onPress={() => router.push("/(auth)/login")}
+      />
+      <ContinuerButton
+        filled={false}
+        text={"Passer"}
+        onPress={() => router.push("/(auth)/login")}
+      />
+    </SafeAreaView>
+  );
+}
+
+export default index;
--- src/components/continuerButton.tsx
+++ src/components/continuerButton.tsx
@@ -1,46 +1,48 @@
-import React from 'react'
-import { StyleSheet, Text } from 'react-native'
-import { TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
 
 interface ContinuerButtonProps {
-  filled ?: boolean
-  text : string
+  filled?: boolean;
+  text: string;
+  onPress?: () => void;
 }
-export default function ContinuerButton(props:ContinuerButtonProps) {
-  return (
-    <TouchableOpacity 
-      style = {[styles.container, props.filled && styles.filled,!props.filled && styles.unfilled] }  >
-      <Text style = {[styles.text,!props.filled && styles.textunfilled
-      ]}  >{props.text} </Text>
-    </TouchableOpacity>
 
-    
-  )
+export default function ContinuerButton(props: ContinuerButtonProps) {
+  return (
+    <TouchableOpacity
+      onPress={props.onPress}
+      style={[
+        styles.container,
+        props.filled && styles.filled,
+        !props.filled && styles.unfilled,
+      ]}
+    >
+      <Text style={[styles.text, !props.filled && styles.textunfilled]}>
+        {props.text}{" "}
+      </Text>
+    </TouchableOpacity>
+  );
 }
+
 const styles = StyleSheet.create({
-  container:{
-    width:"70%",
+  container: {
+    width: "70%",
     paddingHorizontal: 50,
     paddingVertical: 10,
     alignItems: "center",
-    borderRadius:15
-
+    borderRadius: 15,
   },
   text: {
     color: "white",
-    fontWeight:"bold",
-    fontSize:20
-
+    fontWeight: "bold",
+    fontSize: 20,
   },
   filled: {
-    backgroundColor:"#FF6900",
+    backgroundColor: "#FF6900",
   },
   unfilled: {
-    backgroundColor: "#e1e0e0"
+    backgroundColor: "#e1e0e0",
   },
-  textunfilled :{
-    color: "#777777"
-  }
-
-
-})
+  textunfilled: {
+    color: "#777777",
+  },
+});
