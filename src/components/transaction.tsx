18cb3b1da7b4535f3815d73c38a4335fd4f16b93
Benrango <srangoelie@gmail.com>
2026-07-14 04:35:03 +0000

    feat: implement transaction list in wallet component with detailed display

--- src/components/transaction.tsx
+++ src/components/transaction.tsx
@@ -1,9 +1,56 @@
-import { View } from "react-native";
-
-export default function Transaction() {
+import Feather from '@expo/vector-icons/Feather';
+import { StyleSheet, Text, View } from "react-native";
+export interface TransactionProps {
+    id: string
+    title: string;
+    amount: number;
+    date: string;
+    type: 'credit' | 'debit';
+}
+export default function Transaction(props: TransactionProps) {
   return (
-    <View>
-        
+    <View style={styles.container}>
+        <View style={styles.iconContainer}>
+            {
+                props.type === "credit"?  
+                <Feather name="arrow-down-left" size={32} color="green" /> : 
+                <Feather name="arrow-up-right" size={32} color="red" />
+            }
+        </View>
+        <View>
+            <Text style={styles.title} >{props.title}</Text>
+            <Text style={styles.date}>{props.date}</Text>
+        </View>
+        <View style={{ marginLeft: 'auto' }}>
+            <Text style={[{ color: props.type === 'credit' ? 'green' : 'red' }, styles.amount]}>{props.type === 'credit' ? '+' : '-'}{props.amount} FCFA</Text>
+        </View>
     </View>
   )
 }
+const styles = StyleSheet.create({
+    container: {
+        borderRadius: 20,
+        display: 'flex',
+        flexDirection: 'row',
+        height: 80,
+        paddingHorizontal: 15,
+        backgroundColor: "#ffffff",
+        alignItems: "center",
+        gap: 10
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: 600,
+
+    },
+    date: {
+        fontSize: 12
+    },
+    amount:{
+        fontSize: 16,
+        fontWeight: 500,
+    },
+    iconContainer: {
+
+    }
+})
